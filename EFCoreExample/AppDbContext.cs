e8c437dcffaa768297f365f1aabc9b91293896cb
InCorCadit16 <incorcadit16@gmail.com>
2020-03-12 14:28:06 +0400

    initial commit 2

---
+++ EFCoreExample/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreExample
+{
+    class AppDbContext : DbContext
+    {
+        private const string ConnectionString =
+            @"Server = DESKTOP-JI6RI4R;
+                Database = EfCoreDb;
+                Trusted_Connection=true";
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ EFCoreExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EFCoreExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void ListAll()
+        {
+            using (var db = new AppDbContext())
+            {
+                foreach (var book in db.Books.AsQueryable()) 
+                {
+                    var webUrl = book.Author.WebUrl == null? "No web URL given": book.Author.WebUrl;
+                    Console.WriteLine($"{book.Title} by {book.Author.Name}");
+                    Console.WriteLine($"Published on {book.PublishedOn:dd-MMM-yyyy}. {webUrl}");
+
+                }
+            }
+        }
+    }
+}
